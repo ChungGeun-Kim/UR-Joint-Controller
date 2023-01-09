ec1727abed900bf4d8db61e5d15485ad366d210c
ChungGeun Kim <20216033@kumoh.ac.kr>
2023-01-09 15:22:53 +0900

    updated

--- src/Universal_Robots_ROS_Driver/ur_robot_driver/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kcg/catkin_ws/src/Universal_Robots_ROS_Driver/ur_robot_driver
-# Build directory: /home/kcg/catkin_ws/src/Universal_Robots_ROS_Driver/ur_robot_driver/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(_ctest_ur_robot_driver_rostest_test_driver.test "/home/kcg/catkin_ws/src/Universal_Robots_ROS_Driver/ur_robot_driver/build/catkin_generated/env_cached.sh" "/usr/bin/python2" "/opt/ros/melodic/share/catkin/cmake/test/run_tests.py" "/home/kcg/catkin_ws/src/Universal_Robots_ROS_Driver/ur_robot_driver/build/test_results/ur_robot_driver/rostest-test_driver.xml" "--return-code" "/usr/bin/python2 /opt/ros/melodic/share/rostest/cmake/../../../bin/rostest --pkgdir=/home/kcg/catkin_ws/src/Universal_Robots_ROS_Driver/ur_robot_driver --package=ur_robot_driver --results-filename test_driver.xml --results-base-dir \"/home/kcg/catkin_ws/src/Universal_Robots_ROS_Driver/ur_robot_driver/build/test_results\" /home/kcg/catkin_ws/src/Universal_Robots_ROS_Driver/ur_robot_driver/test/driver.test ")
-subdirs("gtest")
